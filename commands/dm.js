06e324d10a0054c32d484cf948f6b431cdfd4741
nathaniscoderman <95654655+4e3@users.noreply.github.com>
2021-12-13 21:59:00 +0000

    descriptive perms

--- commands/dm.js
+++ commands/dm.js
@@ -8,7 +8,15 @@ module.exports = {
     execute(client, message, args, cmd, Discord) {
 
       const lol = client.emojis.cache.find(emoji => emoji.name === "bruhimagine");
-      if (message.author.id !== "572545821849944076") return;
+      if (!'572545821849944076'.includes(message.author.id)) {
+    message.delete()
+    var evalnoEmbed = new Discord.MessageEmbed()
+ .setColor('RED')
+ .setDescription('Due to security issues, dm can be only used by developers.')
+
+ message.channel.send(evalnoEmbed).then(a => a.delete({ timeout: 5000 }))
+    
+  } else {
       var dmrseason = args.slice(1).join(" ");
       const dmtarget = message.mentions.users.first();
 
@@ -18,5 +26,5 @@ module.exports = {
       dmtarget.send(dmrseason)
       message.channel.send("ez done")
      }
-    
+    }
 }
