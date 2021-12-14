834749ab8a9588628a8059c8e075192b21a0303d
nathaniscoderman <95654655+4e3@users.noreply.github.com>
2021-12-14 17:23:07 +0000

    timestamps

--- commands/uptime.js
+++ commands/uptime.js
@@ -5,11 +5,15 @@ module.exports = {
     description: 'sadasclears messages!',
     permissions: [],
     async execute(client, message, args, cmd, Discord) {
-      var uptime = new Date(message.client.uptime);
-      const days = Math.floor(message.client.uptime / (60 * 1000 * 60 * 24));
+
+      const uptime = (new Date() / 1000 - client.uptime / 1000).toFixed();
+
+      //var uptime = new Date(message.client.uptime);
+      //const days = Math.floor(message.client.uptime / (60 * 1000 * 60 * 24));
       var uptimeEmbed = new Discord.MessageEmbed()
  .setColor('GREEN')
- .setDescription(`I have been up for ${days} days, ${uptime.getHours()} hours, ${uptime.getMinutes()} minutes, ${uptime.getSeconds()} seconds.`)
+ .setDescription(`Ping: \`${Math.round(client.ws.ping)}\`ms\nUp since: <t:${uptime}:R>`)
+ //.setDescription(`I have been up for ${days} days, ${uptime.getHours()} hours, ${uptime.getMinutes()} minutes, ${uptime.getSeconds()} seconds.`)
     
  message.channel.send(uptimeEmbed)
 
